689d95e08f98cef963319f2e37fe3f5ea9f5ba63
Ardis Lu <ardisglu@gmail.com>
2022-01-26 22:27:11 -0800

    Add glob support

--- main.ts
+++ main.ts
@@ -1,24 +1,9 @@
-import { parse } from "https://deno.land/std@0.122.0/flags/mod.ts";
 import * as log from "https://deno.land/std@0.122.0/log/mod.ts";
+import { parse } from "https://deno.land/std@0.122.0/flags/mod.ts";
+import { expandGlob } from "https://deno.land/std@0.122.0/fs/mod.ts";
 
-// Example: deno run -A ./main.ts --dir ./example-directory --exec example1.js,example2.js
-let { recordingsDir, recordingsToExecute } = parse(Deno.args, {
-  alias: {
-    'recordingsDir': ['dir', 'D'],
-    'recordingsToExecute': ['exec', 'E']
-  },
-  default: {
-    'recordingsDir': './recordings',
-    'recordingsToExecute': []
-  }
-});
-
-if (!Array.isArray(recordingsToExecute)) {
-  recordingsToExecute = recordingsToExecute.split(',')
-}
-
+/* Set up logger configurations and initialize the logger */
 const startTime = Date.now();
-
 const logFormatter = (r: log.LogRecord) => `${r.datetime.toISOString()} | ${r.levelName} | ${r.msg}`;
 await log.setup({
   handlers: {
@@ -44,37 +29,74 @@ await log.setup({
 
 const logger = log.getLogger();
 
-// Compare actualRecordings with recordingsToExecute and only add valid recordings
-let recordings: string[] = [];
-const actualRecordings = Array.from(Deno.readDirSync(recordingsDir)).map(e => e.name);
-if (recordingsToExecute.length === 0) { // Empty recordingsToExecute --> execute all recordings
-  recordings = actualRecordings;
+/* Parse input flags and handle errors */
+const { dirArg, execArg } = parse(Deno.args, {
+  alias: {
+    'dirArg': ['dir', 'D'],
+    'execArg': ['exec', 'E']
+  },
+  default: {
+    'dirArg': './recordings',
+    'execArg': '*.js'
+  }
+});
+
+if (Array.isArray(dirArg)) {
+  logger.error(`Multiple --dir inputs are not supported. To use recordings from different directories, specify a common root
+    directory with the --dir flag and then pass paths to the separate recording directories using the --exec flag. For example: 
+    --dir ./common-root --exec path/to/example1/*.js --exec path/to/example2/*.js`);
+  Deno.exit(1);
+}
+
+const recordingRootDirectory = await Deno.realPath(dirArg).catch(_ => {
+  logger.error(`Could not resolve --dir. Check that this directory exists: ${dirArg}`);
+  Deno.exit(1);
+});
+
+const execGlobs = [];
+if (Array.isArray(execArg)) {
+  execArg.forEach(a => execGlobs.push(...a.split(',')));
 }
 else {
-  for (const rec of recordingsToExecute) {
-    if (actualRecordings.includes(rec)) {
-      recordings.push(rec);
+  execGlobs.push(...execArg.split(','));
+}
+
+/* Resolve all recording file paths */
+const recordings = [];
+for (const glob of execGlobs) {
+  if (glob === '') { // Will happen if a user mistakenly includes whitespace between args without quoting, e.g. --exec example1.js, example2.js
+    logger.warning(`Ignoring empty string passed to --exec.
+      When passing multiple files to the --exec flag, make sure the argument is wrapped in quotes. For example: --exec "example1.js, example2.js".
+      Note that leading whitespace is NOT trimmed from file names, e.g. the example above will look for a file named " example2.js".`);
+    continue;
+  }
+  let atLeastOneFileResolved = false;
+  for await (const file of expandGlob(`${recordingRootDirectory}/${glob}`)) {
+    if (file.isFile) {
+      recordings.push(file.path);
+      atLeastOneFileResolved = true;
     }
     else {
-      logger.warning(
-        `No recording named "${rec}" in "${recordingsDir}".`,
-      );
+      logger.warning(`${file.path} is not a file.`);
     }
+  }
+  if (!atLeastOneFileResolved) {
+    logger.warning(`"${recordingRootDirectory}/${glob}" did not resolve to any files. Skipping --exec input "${glob}".`);
   }
 }
 
 if (recordings.length === 0) {
   logger.error(
-    `No recordings to execute. Check these configurations are correct:
-      recordingsDir: ${recordingsDir}
-      recordingsToExecute: ${recordingsToExecute}`,
-  );
+    `No recordings to execute. Check these flags are correct and resolve to valid files:
+      --dir: ${dirArg}
+      --exec: ${execArg}`);
+  Deno.exit(1);
 }
 
 for (const rec of recordings) {
   logger.info(`Opened ${rec}`);
   const proc = Deno.run({
-    cmd: ["deno", "run", "-A", "--unstable", `${recordingsDir}/${rec}`],
+    cmd: ["deno", "run", "-A", "--unstable", rec],
   });
   proc.status().then((status) =>
     logger.info(`Closed ${rec} with code ${status.code}.`)
